55071374f6543efa8e16fd8cd26da60fec2bc8e8
ethan.perruzza <ethan.perruzza@epita.fr>
2023-01-11 08:17:14 +0200

    added syncfusion calendar package + basic calendar display

---
+++ lib/widget/calendar_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class CalendarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SfCalendar();
+  }
+}
