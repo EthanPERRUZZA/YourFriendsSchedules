307f1e665b3013314db27d376e8fefd7fe881167
ethan.perruzza <ethan.perruzza@epita.fr>
2023-01-12 20:43:57 +0200

    added style for the event display

--- lib/widget/calendar_widget.dart
+++ lib/widget/calendar_widget.dart
@@ -13,6 +13,33 @@ class CalendarWidget extends StatelessWidget {
     return SfCalendar(
       view: CalendarView.day,
       dataSource: EventDataSource(events),
+      appointmentBuilder: appointmentBuilder, //the style of each event
+      onTap: (details) {}, //To get more details on the event (onclick)
+    );
+  }
+
+  Widget appointmentBuilder(
+    BuildContext context,
+    CalendarAppointmentDetails details,
+  ) {
+    final event = details.appointments.first;
+
+    return Container(
+      width: details.bounds.width,
+      height: details.bounds.height,
+      decoration: BoxDecoration(
+          color: event.backgroundColor.withOpacity(0.7),
+          borderRadius: BorderRadius.circular(6)),
+      padding: EdgeInsets.all(3.0),
+      child: Text(
+        event.title,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
