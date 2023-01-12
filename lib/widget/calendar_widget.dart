f7b87aa15b763c1b50c4b09bf9d3ef10f1964989
ethan.perruzza <ethan.perruzza@epita.fr>
2023-01-12 20:18:46 +0200

    event display on calendar

--- lib/widget/calendar_widget.dart
+++ lib/widget/calendar_widget.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import '../provider/event_provider.dart';
+import '../model/event_data_source.dart';
 
 class CalendarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Loads all the events already created
+    final events = Provider.of<EventProvider>(context).events;
+
     return SfCalendar(
       view: CalendarView.day,
+      dataSource: EventDataSource(events),
     );
   }
 }
